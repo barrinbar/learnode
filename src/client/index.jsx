61d8696076a22df2d24682f01887f17e995f4df3
barrinbar <barrinbar@gmail.com>
2017-05-14 15:34:42 +0300

    Style with JSS

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,5 +1,8 @@
 // @flow
 
+import $ from 'jquery'
+import Tether from 'tether'
+
 import 'babel-polyfill'
 import Immutable from 'immutable'
 import React from 'react'
@@ -12,9 +15,13 @@ import { BrowserRouter } from 'react-router-dom'
 
 import App from '../shared/app'
 import helloReducer from '../shared/reducer/hello'
-import { APP_CONTAINER_SELECTOR } from '../shared/config'
+import { APP_CONTAINER_SELECTOR, JSS_SSR_SELECTOR } from '../shared/config'
 import { isProd } from '../shared/util'
 import setUpSocket from './socket'
+
+window.jQuery = $
+window.Tether = Tether
+require('bootstrap')
 
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
@@ -47,5 +54,9 @@ if (module.hot) {
     ReactDOM.render(wrapApp(NextApp, store), rootEl)
   })
 }
+
+const jssServerSide = document.querySelector(JSS_SSR_SELECTOR)
+// flow-disable-next-line
+jssServerSide.parentNode.removeChild(jssServerSide)
 
 setUpSocket(store)
